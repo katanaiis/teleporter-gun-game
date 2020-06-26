2b14d92a54199e2bd2ff7d0d2dc67161c0003549
Anais Van Den Houtaer <a.houtaer@interface3.be>
2020-06-26 14:05:55 +0200

    add teleporter

---
+++ Teleporter gun game/teleporter-gun-game/Assets/#Project/Scripts/FireGun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject projectile;
+    public Transform spawnPoint;
+
+    void Start()
+    {
+        
+    }
+
+    private void Fire()
+    {
+        Vector3 position = spawnPoint.position;
+        Quaternion rotation = spawnPoint.rotation;
+
+        Instantiate(projectile, position, rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+}
---
+++ Teleporter gun game/teleporter-gun-game/Assets/#Project/Scripts/LifeTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float time;
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+
+}
