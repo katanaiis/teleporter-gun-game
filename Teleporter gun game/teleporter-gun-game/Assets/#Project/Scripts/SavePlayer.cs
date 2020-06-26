265d0c9294a1492adeeadf8b3ffe15e96a7d1eff
Anais Van Den Houtaer <a.houtaer@interface3.be>
2020-06-26 15:00:50 +0200

    add first level

---
+++ Teleporter gun game/teleporter-gun-game/Assets/#Project/Scripts/SavePlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject player;
+    private Vector3 originalPosition;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        originalPosition = player.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            CharacterController cc = player.GetComponent<CharacterController>();
+            cc.enabled = false;
+            player.transform.position = originalPosition;
+            cc.enabled = true;
+        }
+    }
+
+}
